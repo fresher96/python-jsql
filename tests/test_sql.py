bf06e3b8f1b8b0d84e9a27051233c13415152943
Farouk Hjabo <fhjabo@noon.com>
2024-08-25 18:55:08 +0400

    use monkeypatch for sql_inner test

--- tests/test_sql.py
+++ tests/test_sql.py
@@ -14,19 +14,23 @@ def logparams_decorator(fn):
     @functools.wraps(fn)
     def inner(engine, template, params):
         template = template.replace('HOOKPARAMS', '/* pod=xyz */')
+        template = template.replace('COMMENTS', '')
         return fn(engine, template, params)
     return inner
 
 engine = object()
-def test_sql():
-    ret = jsql.sql(engine, 'SELECT 1 FROM tbl {% if customer %}WHERE customer=:customer{% endif %}', customer='abc').dict()
-    assert ret['engine'] == engine
-    assert ret['query'] == 'SELECT 1 FROM tbl WHERE customer=:customer'
-    assert ret['params']['customer'] == 'abc'
 
-def test_sqlhook():
-    jsql.sql_inner = logparams_decorator(jsql.sql_inner)
+
+def test_sqlhook(monkeypatch):
+    monkeypatch.setattr(jsql, jsql.sql_inner.__name__, logparams_decorator(jsql.sql_inner))
+    # jsql.sql_inner = logparams_decorator(jsql.sql_inner)
     ret = jsql.sql(engine, 'SELECT 1 FROM tbl WHERE 1=1 HOOKPARAMS').dict()
     assert ret['query'] == 'SELECT 1 FROM tbl WHERE 1=1 /* pod=xyz */'
 
+
+def test_sql():
+    ret = jsql.sql(engine, 'SELECT 1 FROM tbl {% if customer %}WHERE customer=:customer{% endif %} COMMENTS', customer='abc').dict()
+    assert ret['engine'] == engine
+    assert ret['query'] == 'SELECT 1 FROM tbl WHERE customer=:customer COMMENTS'
+    assert ret['params']['customer'] == 'abc'
 
